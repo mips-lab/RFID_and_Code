90e04d413a408ae97ce17f984d4a0072b3d04fea
Antoine VAN-ELSTRAETE <antoine@van-elstraete.net>
2014-07-10 22:15:32 +0200

    version de dev 2014-07-10

--- rfid_and_code/rfid_and_code.ino
+++ rfid_and_code/rfid_and_code.ino
@@ -1,3 +1,4 @@
+#include <Keypad.h>
 #include <SoftwareSerial.h>
  
 SoftwareSerial SoftSerial(12, 13);
@@ -6,50 +7,69 @@ unsigned char buffer2[4];
 unsigned long currentTime;
 unsigned long loopTime;
 int count=0;     // counter for buffer array 
+const byte ROWS = 4; //four rows
+const byte COLS = 3; //three columns
+char keys[ROWS][COLS] = {
+  {'1','2','3'},
+  {'4','5','6'},
+  {'7','8','9'},
+  {'*','0','#'}
+};
+byte rowPins[ROWS] = {5, 4, 3, 2}; //connect to the row pinouts of the keypad
+byte colPins[COLS] = {8, 7, 6}; //connect to the column pinouts of the keypad
+Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
+char RFID;
+
 void setup()
 {
-  currentTime = millis();
-  loopTime = currentTime;
-  SoftSerial.begin(9600);               // the SoftSerial baud rate   
-  Serial.begin(9600);             // the Serial port of Arduino baud rate.
- 
-}
- 
-void loop()
-{
-  if (SoftSerial.available())              // if date is comming from softwareserial port ==> data is comming from SoftSerial shield
-  {
-    while(SoftSerial.available())          // reading data into char array 
-    {
-      buffer[count++]=SoftSerial.read();     // writing data into array
-      if(count == 64)break;
-  }
-    Serial.write(buffer,count);            // if no data transmission ends, write buffer to hardware serial port
-    clearBufferArray();              // call clearBufferArray function to clear the storaged data from the array
-    count = 0;                       // set counter of while loop to zero
- 
-  }
-  currentTime = millis();
-  while(currentTime >= (loopTime + 30000)){
-    if (Serial.available()){
-      buffer2[count++]=Serial.read();          // if data is available on hardwareserial port ==> data is comming from PC or notebook
-      if(count == 4)break;
-    }
-    Serial.write(buffer2,count);
-    clearBuffer2Array();
-    count = 0;
-  }
-  
+	currentTime = millis();
+	loopTime = currentTime;
+	SoftSerial.begin(9600);               // the SoftSerial baud rate   
+	Serial.begin(9600);             // the Serial port of Arduino baud rate.
 }
 
 void clearBufferArray()              // function to clear buffer array
 {
-  for (int i=0; i<count;i++)
-    { buffer[i]=NULL;}                  // clear all index of array with command NULL
+	for (int i=0; i<count;i++)
+	{ buffer[i]=NULL;}                  // clear all index of array with command NULL
 }
 
 void clearBuffer2Array()              // function to clear buffer array
 {
-  for (int i=0; i<count;i++)
-    { buffer2[i]=NULL;}                  // clear all index of array with command NULL
+	for (int i=0; i<count;i++)
+	{ buffer2[i]=NULL;}                  // clear all index of array with command NULL
+}
+
+ 
+void loop()
+{
+	if (SoftSerial.available())              // if date is comming from softwareserial port ==> data is comming from SoftSerial shiel
+	{
+		while(SoftSerial.available())          // reading data into char array 
+		{
+			buffer[count++]=SoftSerial.read();     // writing data into array
+			if(count == 64)break;
+		}
+		Serial.write(buffer,count);            // if no data transmission ends, write buffer to hardware serial port
+		clearBufferArray();              // call clearBufferArray function to clear the storaged data from the array
+		count = 0;                       // set counter of while loop to zero
+	}
+	currentTime = millis();
+	while(currentTime >= (loopTime + 30000))
+	{
+	if (Serial.available())
+	{
+		buffer2[count++]=Serial.read();          // if data is available on hardwareserial port ==> data is comming from PC or notebook
+		if(count == 4)break;
+	}
+//	Serial.write(buffer2,count);
+	RFID = (buffer2,count);
+    clearBuffer2Array();
+    count = 0;
+	}
+	char key = keypad.waitForKey();
+	if (key){
+		Serial.println(String(RFID) + "&" + String(key));
+	}
+  
 }
